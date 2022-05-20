7bd60c19e139bc70bcd315ebb847ff7855ad9cee
Anns007 <annsshahbaz@gmail.com>
2022-05-20 19:35:32 +0500

    7:35

--- GDSC_Codefest/GDSC_Codefest/Source.cpp
+++ GDSC_Codefest/GDSC_Codefest/Source.cpp
@@ -4,6 +4,7 @@
 #include<vector>
 using namespace std;
 int i = 1;
+int Count = 0;
 int maxCount = -1;
 class Node {
 public:
@@ -18,15 +19,25 @@ public:
 };
 
 void function(Node* root) {
-	if (i < maxCount) {
-		function(root->left);
-		function(root->right);
-		root->value = i++;
-	}
-	else {
-		return;
-	}
+
+	if(root->left)  function(root->left);	
+	if(root->right) function(root->right);
+	root->value = i++;
 }
+
+Node* push(Node* root) {
+	if (!root) {
+		root = new Node(0);
+		return root;
+	}
+	else if (!root->left) {
+		return push(root->left);
+	}
+	else if (!root->right) {
+		return push(root->right);
+	}
+	return NULL;
+}	
 
 int main() {
 
@@ -43,7 +54,14 @@ int main() {
 	}
 	int top = pow(2, h - 1);
 	maxCount = top;
-	Node* root = new Node(top);
+	Node* root = NULL;
+	root = push(root);
+	root = push(root);
+	root = push(root);
+	root = push(root);
+	root = push(root);
+	root = push(root);
+	root = push(root);
 	function(root);
 
 	return 0;
